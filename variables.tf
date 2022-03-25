71f74d4594cb65034dbc51f01d9b182e1661c85e
Xuliang Liu <liuxuliang69@gmail.com>
2022-03-25 00:41:28 +0000

    Added remote backend

--- variables.tf
+++ variables.tf
@@ -7,11 +7,12 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
+  default = "xuliangliu"
 }
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "centralus"
+  default     = "Australia Southeast"
 }
 
 variable "address_space" {
